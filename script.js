e20230d9795ac43a35d7309b920249c1891c8c01
Harsimran singh <simran.9971290436@gmail.com>
2021-09-29 00:52:29 +0530

    create 4*4 grid

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const gridDisplay = document.querySelector(".grid");
+    const scoreDisplay = document.querySelector(".score");
+    const resultDisplay = document.querySelector(".result");
+
+    let width = 4;
+    let squares = [];
+
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            let square = document.createElement('div');
+            square.classList.add("grid-small-squares-box");
+            square.innerHTML = 0;
+            gridDisplay.append(square);
+            squares.push(square);
+        }
+    }
+
+    createBoard();
+
 })
